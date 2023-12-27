b90ca0a93a8c1a594131859876b9270d4d81232e
unsuspected2 <154977360+unsuspected2@users.noreply.github.com>
2023-12-27 13:02:09 +0100

    Add files via upload

---
+++ indesx.js
@@ -0,0 +1,9 @@
+first="Home";
+second="Protofilio";
+third="Sobre mim";
+fourth="Contacto";
+
+document.getElementById("1").innerHTML = first;
+document.getElementById("2").innerHTML = second;
+document.getElementById("3").innerHTML = third;
+document.getElementById("4").innerHTML = fourth;
